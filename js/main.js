24cf006dd438b0d471e8ffaaa9bdfa30c1d8bf5c
fernandodeleon <fernando.deleon@gmail.com>
2017-01-11 23:06:41 +1000

    added carousel and some images

--- js/main.js
+++ js/main.js
@@ -18,6 +18,49 @@
             publicVar: "am public",
             publicMethod: function (name) {
                 console.log(privateMethod(name) + " " + aPrivObj.happy);
+            },
+            initKidsImageCarousel: function () {
+                $("#kidsImages").slick({
+                    dots: true,
+                    infinite: false,
+                    speed: 300,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    centerPadding: '40px',
+                    adaptiveHeight: true,
+                    responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 2,
+                                infinite: true,
+                                dots: true,
+                                centerPadding: '40px',
+                                adaptiveHeight: true
+                            }
+                        },
+                        {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 2,
+                                centerPadding: '20px',
+                                adaptiveHeight: true
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
+                                centerPadding: '5px',
+                                adaptiveHeight: true
+
+                            }
+                        }
+                    ]
+                });
             }
 
         };
@@ -26,10 +69,7 @@
     $(document).ready(function () {
         console.log("document is ready!");
         MyModule.publicMethod("Nando");
-
-
-
-
+        MyModule.initKidsImageCarousel();
 
 
     })
